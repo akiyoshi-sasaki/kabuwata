2d2e31c589f97e73fbe98a6a4886000189af1fad
佐々木祥佳 <myfrovoc@docomo.ne.jp>
2017-11-25 01:07:06 +0900

    twitterのAPIと接続ができる

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # kabuwata
+
+twitter-apiの使い方
+https://cre8cre8.com/python/twitter-api.htm
+
+ 
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello, KABUWATA!";
+require 'TwistOAuth/build/TwistOAuth.phar';
+$consumer_key        = 'G';
+$consumer_secret     = 'M';
+$access_token        = 'r';
+$access_token_secret = '9';
+$connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+var_dump($connection);
+
+echo "Hello, KABUWATA!?";
