296678b0c7512231e285d1649daaa35ded6a987b
佐々木祥佳 <myfrovoc@docomo.ne.jp>
2017-11-25 01:07:06 +0900

    use environment variable

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello, KABUWATA!";
+require 'TwistOAuth.phar';
+// $consumer_key        = 'xxxxxxxxxxx';
+// $consumer_secret     = 'xxxxxxxxxxx';
+// $access_token        = 'xxxxxxxxxxx';
+// $access_token_secret = 'xxxxxxxxxxx';
+// $connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+
+echo ADMIN_USER_NAME;
+echo "Hello, KABUWATA!?";
