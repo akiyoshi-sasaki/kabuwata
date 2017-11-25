8bb2b22afcb83ade7a13000b1a4446013c0be5f1
akiyoshi <myfrovoc@docomo.ne.jp>
2017-11-25 07:34:34 +0000

    初動としてtwitterからハッシュタグでツイート取得できた

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 twitter-apiの使い方
 https://cre8cre8.com/python/twitter-api.htm
 
+# openssl & php_curl
+
+`sudo apt-get install php-curl`
+php.iniのコメントアウトを外す
  
--- index.php
+++ index.php
@@ -8,6 +8,13 @@ $consumer_secret     = TwitterApi::CONSUMER_SECRET;
 $access_token        = TwitterApi::ACCESS_TOKEN;
 $access_token_secret = TwitterApi::ACCESS_TOKEN_SECRET;
 $connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
-var_dump($connection);
+
+// ハッシュタグによるツイート検索
+$hash_params = ['q' => '#majiko' ,'count' => '2', 'lang'=>'ja'];
+$hash = $connection->get('search/tweets', $hash_params)->statuses;
+
+echo "<pre>";
+var_dump($hash);
+echo "</pre>";
 
 echo "Hello, KABUWATA!?";
