ce259a31811e16801cd2fb444f1afce0f357fe85
akiyoshi <myfrovoc@docomo.ne.jp>
2017-11-26 05:07:03 +0000

    サーバ立て直したので色々イレギュラー

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
 
-var_dump($_POST);
-
 require '.twitter.php';
 require 'TwistOAuth/build/TwistOAuth.phar';
 
-$consumer_key        = TwitterApi::CONSUMER_TOKEN;
-$consumer_secret     = TwitterApi::CONSUMER_SECRET;
-$access_token        = TwitterApi::ACCESS_TOKEN;
-$access_token_secret = TwitterApi::ACCESS_TOKEN_SECRET;
-$connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
-
+$consumer_key    = TwitterApi::CONSUMER_KEY;
+$consumer_secret = TwitterApi::CONSUMER_SECRET;
+$access_token    = TwitterApi::ACCESS_TOKEN;
+$access_secret   = TwitterApi::ACCESS_SECRET;
+$connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $access_secret);
 
 if (isset($_POST['keyword'])) {
-
-　　　　$keyword = str_replace(' ', '', $_POST['keyword']);　	
-	$count = (isset($_POST['count'])) ? $_POST['count'] : 10;
+        $keyword = str_replace(' ', '', $_POST['keyword']);
+        $count = (isset($_POST['count'])) ? $_POST['count'] : 10;
 
 	// キーワードによるツイート検索
 	$tweets_params = ['q' => str_replace(',', ' OR ', $keyword), 'count' => $count];
