24b108565b1eb0f69d36f42357b6bc4e50b7348f
佐々木祥佳 <myfrovoc@docomo.ne.jp>
2017-12-09 22:18:07 +0900

    エゴサ検索の開始日付を入力

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require '.twitter.php';
 require 'TwistOAuth/build/TwistOAuth.phar';
 
+var_dump($_POST);
+
 $consumer_key    = TwitterApi::CONSUMER_KEY;
 $consumer_secret = TwitterApi::CONSUMER_SECRET;
 $access_token    = TwitterApi::ACCESS_TOKEN;
@@ -12,10 +14,16 @@ $connection = new TwistOAuth($consumer_key, $consumer_secret, $access_token, $ac
 if (isset($_POST['keyword'])) {
         $keyword = str_replace(' ', '', $_POST['keyword']);
         $count = (isset($_POST['count'])) ? $_POST['count'] : 10;
+				$until = (isset($_POST['until'])) ? $_POST['until'] : date('Y-m-d');
 
 	// キーワードによるツイート検索
-	$tweets_params = ['q' => str_replace(',', ' OR ', $keyword), 'count' => $count];
-	var_dump($tweets_params);
+	$tweets_params = [
+		'q' => str_replace(',', ' OR ', $keyword),
+		'count' => $count,
+		'lang' => 'ja',
+		'result_type' => 'mixed',
+		'until' => $until;
+	];
 	$tweets        = $connection->get('search/tweets', $tweets_params)->statuses;
 
 	foreach($tweets as $tweet){
